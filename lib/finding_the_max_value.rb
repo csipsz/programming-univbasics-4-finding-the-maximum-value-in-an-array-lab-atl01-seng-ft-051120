f5782f3488146cffc684472925e8cff9e263ec89
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-31 15:29:38 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,5 +2,10 @@ def find_max_value(array)
   count = 0 
   max = 0 
   while count < array.length do 
+    if array[count] > max 
+      max = array[count]
+    end
+    count += 1 
   end 
+  max 
 end
